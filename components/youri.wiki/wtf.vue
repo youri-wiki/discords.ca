bdb6491fad2bc269806028a6b715d3b490ae2f82
masterdpro <youri.d2.bel@gmail.com>
2024-09-22 15:37:51 -0500

    workfls

--- components/youri.wiki/wtf.vue
+++ components/youri.wiki/wtf.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wtf-container">
-    <h1>WTF!</h1>
+    <h1>WTF!!</h1>
     <form @submit.prevent="sendNote(username, content)">
       <input type="text" v-model="username" placeholder="Username" />
       <input type="text" v-model="content" placeholder="Content" />
