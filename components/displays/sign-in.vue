75868ae7dff068be4859f7bef9d4883ed1ac7317
masterdpro <youri.d2.bel@gmail.com>
2024-09-25 18:59:02 -0500

    Remake login/signin page

--- components/displays/sign-in.vue
+++ components/displays/sign-in.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="username" placeholder="Username" />
-    <input type="password" v-model="password" placeholder="Password" />
-    <button @click="signin">signin</button>
+  <div class="container">
+    <div class="input-container">
+      <input type="text" v-model="username" placeholder="Username" class="text-input" />
+      <input type="password" v-model="password" placeholder="Password" class="text-input" />
+    </div>
+    <button @click="signin" class="button"><i class="fas fa-sign-in-alt"></i></button>
   </div>
 </template>
 
@@ -51,5 +53,61 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  background-color: rgb(205, 205, 205);
+  display: flex;
+  height: 100px;
+  width: 15rem;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border-radius: 20px;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.text-input {
+  width: 10rem;
+  height: 50%;
+  border-left: 5px solid rgb(0, 0, 0);
+  border-right: 0px solid;
+  border-top: 0px solid;
+  border-bottom: 1px solid rgb(0, 0, 0);
+  background-color: transparent;
+  margin-top: 10px;
+  transition: all 0.3s ease-in-out;
+}
+
+.button {
+  position: relative;
+  height: 100%;
+  background-color: transparent;
+  border: 2px solid rgba(0, 0, 0, 0);
+  border-left: 0px solid transparent;
+  transition: all 0.3s ease-in-out;
+  width: 50px;
+  left: 0;
+}
+
+.button:hover {
+  transition: all 0.3s ease-in-out;
+  rotate: 180deg;
+}
+
+.text-input:focus {
+  outline: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-color: #555 #555 transparent #555;
+
+}
+
 /* Add any custom styles for your login form here */
 </style>
